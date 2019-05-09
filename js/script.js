60a4298abdf3e64d341229471300d7c48fe7372d
Eli Zhang <zhang.eli@hotmail.com>
2019-05-09 09:42:52 -0400

    changed operator symbols

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ operator.forEach(function(math) {
     math.addEventListener('click', function() {
         let string = input.innerHTML;
         const lastChar = string[string.length - 1];
-        if (lastChar === '+' || lastChar === '-' || lastChar === 'x' || lastChar === '/') {
+        if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
             string = string.substring(0, (string.length - 1));
         }
         input.innerHTML = string + this.innerHTML;
