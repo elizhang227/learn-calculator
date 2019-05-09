5974e2413e968616eb1aef6532c8ea91fefc276b
Eli Zhang <zhang.eli@hotmail.com>
2019-05-09 10:40:59 -0400

    added conditional for numbers to check if result has been displayed or not

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// square.addEventListener('click', function() {
-//     const name = promptForName();
-//     sayHello(name);
-// });
-
 const input = document.getElementById('input'), // input/output button
     number = document.querySelectorAll('.numbers div'), // number buttons
     operator = document.querySelectorAll('.operators div'), // operator buttons
@@ -14,10 +9,31 @@ const input = document.getElementById('input'), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-number.forEach(function(numero) {
+// number.forEach(function(numero) {
+//     numero.addEventListener('click', function() {
+//         input.innerHTML += this.innerHTML;
+//     });
+// });
+Array.from(number).map(numero => {
     numero.addEventListener('click', function() {
-        input.innerHTML += this.innerHTML;
-    });
+        let string = input.innerHTML;
+        const lastChar = string[string.length - 1];
+
+        if (resultDisplayed === false) {
+            input.innerHTML += this.innerHTML;
+        }
+        else if (
+            resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"
+        )   {
+                resultDisplayed = false;
+                input.innerHTML += this.innerHTML;
+            }
+        else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+        }
+    })
 });
 
 // adding click handlers to the operation buttons
@@ -27,13 +43,35 @@ operator.forEach(function(math) {
         const lastChar = string[string.length - 1];
         if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
             string = string.substring(0, (string.length - 1));
+            input.innerHTML = string + this.innerHTML;
         }
-        input.innerHTML = string + this.innerHTML;
+        else {
+            input.innerHTML += this.innerHTML;
+        }
     });
 });
 
 // on click of 'equal' button
+// Array.from(number).map(numero => {
+//     numero.addEventListener('click', function() {
+//         if (resultDisplayed === false) {
+//             input.innerHTML += this.innerHTML;
+//         }
+//         else if ((resultDisplayed === true && lastChar === "+") ||
+//                 (resultDisplayed === true && lastChar === "-") ||
+//                 (resultDisplayed === true && lastChar === "*") ||
+//                 (resultDisplayed === true && lastChar === "/")) {
 
+//                 resultDisplayed = false;
+//                 input.innerHTML += this.innerHTML;
+//                 }
+//         else {
+//             resultDisplayed = false;
+//             input.innerHTML = "";
+//             input.innerHTML += this.innerHTML;
+//         }
+//     })
+// });
 
 // clearing the input on press of clear
 clear.addEventListener('click', function() {
